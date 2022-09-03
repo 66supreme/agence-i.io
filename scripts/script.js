31210ef63ed8143d0524cd5cbb41ad05a60d5f29
oumar <57168005+Abib-web@users.noreply.github.com>
2022-09-03 18:01:09 -0400

    modification du 03/09

---
+++ scripts/script.js
@@ -0,0 +1,55 @@
+// JavaScript source code
+/*PART1*/
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $('.nav');
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+
+    });
+});
+
+var menu = document.getElementById("bar");
+var items = document.getElementById("navbar");
+
+items.style.right = "-360px";
+
+menu.onclick = function () {
+    if (items.style.right == "-360px") {
+        items.style.right = "0";
+    }
+    else {
+        items.style.right = "-360px";
+
+    }
+}
+
+
+/*PART2*/
+$('.first-name-class').on('input', function () {
+    // do something
+    if (String($('.first-name-class').val()).length > 0) {
+        $('#start-fisrt-name').hide();
+
+    } else {
+        $('#start-fisrt-name').show();
+    }
+});
+$('.name-class').on('input', function () {
+    // do something
+    if (String($('.name-class').val()).length > 0) {
+        $('#start-name').hide();
+
+    } else {
+        $('#start-name').show();
+    }
+});
+
+$('.mail-class').on('input', function () {
+    // do something
+    if (String($('.mail-class').val()).length > 0) {
+        $('#start-mail').hide();
+
+    } else {
+        $('#start-mail').show();
+    }
+});
